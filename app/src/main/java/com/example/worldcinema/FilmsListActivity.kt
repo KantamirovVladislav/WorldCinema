3b791efd635c93bcb737b550af7de22b40fbe756
kanta <kantamirov2016@gmail.com>
2024-10-23 10:00:09 +0300

    dsa

--- app/src/main/java/com/example/worldcinema/FilmsListActivity.kt
+++ app/src/main/java/com/example/worldcinema/FilmsListActivity.kt
@@ -6,12 +6,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.worldcinema.remote.Movie
-import com.example.worldcinema.remote.RetrofitClient
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Call
-
 class FilmsListActivity : AppCompatActivity() {
 
     private val apiKey = "T2TXQGQ-DBH49R4-KE9QNDM-P5RE17J"
@@ -25,28 +19,5 @@ class FilmsListActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        getRandomMovie()
-    }
-
-    private fun getRandomMovie() {
-        val call = RetrofitClient.api.getRandomMovie()
-
-        call.enqueue(object : Callback<Movie> {
-            override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
-                if (response.isSuccessful) {
-                    val movie = response.body()
-                    movie?.let {
-                        Log.d("MovieData", it.toString())
-                    }
-                } else {
-                    Log.e("APIError", "Response code: ${response.code()}")
-                }
-            }
-
-            override fun onFailure(call: Call<Movie>, t: Throwable) {
-                Log.e("APIError", "Request failed: ${t.message}")
-            }
-        })
     }
 }
